f37e322430979c2bc31392b27c7c87b9442d0811
Hiba Oualem <hibaoualen@gmail.com>
2023-04-25 07:58:52 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,6 @@
 #include "main.h"
+
+
 /**
  * main - print _putchar
  * is written by mbah
@@ -6,16 +8,16 @@
  */
 
 
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
-}	
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
