86734c7b03f8a3a1afd3114c3676791bf730aaa7
Hiba Oualem <hibaoualen@gmail.com>
2023-05-17 12:29:41 -0700

    statics

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,11 +1,6 @@
 #include "main.h"
 
-/**
- * _puts - writes the sentence
- * Return: On success 0.
- */
-
-int main(void);
+int main(void)
 {
 	_puts("\"Attheendoftheday,mygoalwastobethebesthacker\"\n\t-KevinMitnick");
 	return (0);
